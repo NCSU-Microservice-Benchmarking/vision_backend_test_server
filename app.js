557a734b9a5960f140a992b7c25bd52f270f3b48
zli85 <zli85@ncsu.edu>
2023-10-20 17:44:19 -0400

    add response check for detection

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const axios = require('axios'); // Import Axios
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 
+
 const app = express();
-const port = 5001;
+const port = 8001;
 const host = '0.0.0.0'
 
 // Middleware to parse JSON and form data
@@ -34,21 +36,27 @@ app.get('/greet', (req, res) => {
 
 
 // Endpoint to handle the POST request
+// example: http://localhost:8001/tests?task=detection&port=5000&api=detections
 app.post('/tests', upload.single('image'), (req, res) => {
     // Retrieve query parameters
     const task = req.query.task;
     const port = req.query.port;
     const api = req.query.api;
 
+    console.log('Received a request from ' + req.connection.remoteAddress);
+    console.log('Task: ' + task);
+    console.log('Port: ' + port);
+    console.log('API: ' + api);
+
     // Ensure the task is "detect"
-    if (task == 'detection') {
+    if (task == 'detection' || task == 'segmentation') {
         // Get the path to the image, which is "test_images/detect.png"
         const imagePath = path.join(__dirname, 'test_images', 'detect.png');
         const requesterIP = req.connection.remoteAddress; // Get the requester's IP
 
         // Create a readable stream for the image
         const imageStream = fs.createReadStream(imagePath);
-
+        
         // Configure the HTTP POST request
         const requestOptions = {
             url: `http://${requesterIP}:${port}/${api}`, // Assuming you want to send it to the requester's IP on port 5000
@@ -63,9 +71,69 @@ app.post('/tests', upload.single('image'), (req, res) => {
                 return res.status(500).json({ error: 'Failed to send the image to the requester' });
             }
 
-            res.status(200).json({ message: 'Image sent to the requester' });
+            // check the format of the response file
+            // if the response is a image encoded in binary format, return true with code 200
+            // else return false with code 400
+            if (response.headers['content-type'] == 'image/png') {
+                // get the image file
+                const imageFile = Buffer.from(body, 'binary');
+                // check if the image is valid
+                if (imageFile.length == 0) {
+                    console.log('Invalid image file size');
+                    return res.status(400).json({ error: 'Invalid image file size' });
+                }
+                console.log('received image format: ' + response.headers['content-type'] + ', size: ' + imageFile.length);
+                return res.status(200).json({ message: 'Image sent to the requester' });
+            }
+            else return res.status(400).json({ error: 'Invalid image format' });
+        });
+
+
+
+    }
+    else if (task == 'inpainting') {
+        // Get the path to images, which is "test_images/inpainting_image.png" and "test_images/inpainting_mask.png"
+        const imagePath = path.join(__dirname, 'test_images', 'inpainting_image.png');
+        const maskPath = path.join(__dirname, 'test_images', 'inpainting_mask.png');
+        const requesterIP = req.connection.remoteAddress; // Get the requester's IP
+
+        // Create a readable stream for the image
+        const imageStream = fs.createReadStream(imagePath);
+        const maskStream = fs.createReadStream(maskPath);
+
+        // Configure the HTTP POST request
+        const requestOptions = {
+            url: `http://${requesterIP}:${port}/${api}`, // Assuming you want to send it to the requester's IP on port 5000
+            formData: {
+                image: imageStream,
+                mask: maskStream,
+            },
+        };
+
+        // Send the HTTP POST request to the requester's IP with the image
+        request.post(requestOptions, (error, response, body) => {
+            if (error) {
+                return res.status(500).json({ error: 'Failed to send the image to the requester' });
+            }
+
+            // check the format of the response file
+            // if the response is a image encoded in binary format, return true with code 200
+            // else return false with code 400
+            if (response.headers['content-type'] == 'image/png') {
+                // get the image file
+                const imageFile = Buffer.from(body, 'binary');
+                // check if the image is valid
+                if (imageFile.length == 0) {
+                    console.log('Invalid image file size');
+                    return res.status(400).json({ error: 'Invalid image file size' });
+                }
+                console.log('received image format: ' + response.headers['content-type'] + ', size: ' + imageFile.length);
+                return res.status(200).json({ message: 'Image sent to the requester' });
+            }
+            else return res.status(400).json({ error: 'Invalid image format' });
         });
     }
+
     else return res.status(400).json({ error: 'Invalid task specified' });
 });
 
