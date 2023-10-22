5fee99c5607697431981f079a5cb88bb639dd1cf
zli85 <zli85@ncsu.edu>
2023-10-22 17:11:27 -0400

    update image upload format

--- app.js
+++ app.js
@@ -37,14 +37,16 @@ app.get('/greet', (req, res) => {
 
 // Endpoint to handle the POST request
 // example: http://localhost:8001/tests?task=detection&port=5000&api=detections
-app.post('/tests', upload.single('image'), (req, res) => {
+app.get('/tests', upload.single('image'), async (req, res) => {
     // Retrieve query parameters
     const task = req.query.task;
+    const target_host = req.query.host;
     const port = req.query.port;
     const api = req.query.api;
 
     console.log('Received a request from ' + req.connection.remoteAddress);
     console.log('Task: ' + task);
+    console.log('Host: ' + target_host);
     console.log('Port: ' + port);
     console.log('API: ' + api);
 
@@ -52,31 +54,31 @@ app.post('/tests', upload.single('image'), (req, res) => {
     if (task == 'detection' || task == 'segmentation') {
         // Get the path to the image, which is "test_images/detect.png"
         const imagePath = path.join(__dirname, 'test_images', 'detect.png');
-        const requesterIP = req.connection.remoteAddress; // Get the requester's IP
 
         // Create a readable stream for the image
-        const imageStream = fs.createReadStream(imagePath);
-        
-        // Configure the HTTP POST request
-        const requestOptions = {
-            url: `http://${requesterIP}:${port}/${api}`, // Assuming you want to send it to the requester's IP on port 5000
-            formData: {
-                image: imageStream,
-            },
-        };
+        // const imageStream = fs.createReadStream(imagePath);
+        const imageStream = fs.readFileSync(imagePath);
 
-        // Send the HTTP POST request to the requester's IP with the image
-        request.post(requestOptions, (error, response, body) => {
-            if (error) {
-                return res.status(500).json({ error: 'Failed to send the image to the requester' });
+        const serviceURL = `http://${target_host}:${port}/${api}`;
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
             }
+        }
 
-            // check the format of the response file
-            // if the response is a image encoded in binary format, return true with code 200
-            // else return false with code 400
+        // use axios to post the form data to the serviceURL
+        try {
+            const formData = new FormData();
+            formData.append('image', imageStream);
+
+            console.log('imageStream: ' + imageStream);
+
+            const response = await axios.post(serviceURL, formData, config);
+
             if (response.headers['content-type'] == 'image/png') {
                 // get the image file
-                const imageFile = Buffer.from(body, 'binary');
+                const imageFile = Buffer.from(response.data, 'binary');
                 // check if the image is valid
                 if (imageFile.length == 0) {
                     console.log('Invalid image file size');
@@ -86,10 +88,10 @@ app.post('/tests', upload.single('image'), (req, res) => {
                 return res.status(200).json({ message: 'Image sent to the requester' });
             }
             else return res.status(400).json({ error: 'Invalid image format' });
-        });
-
-
-
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: 'Internal Error, failed to send the image to the requester' });
+        }
     }
     else if (task == 'inpainting') {
         // Get the path to images, which is "test_images/inpainting_image.png" and "test_images/inpainting_mask.png"
